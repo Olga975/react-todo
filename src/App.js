c4c19b55879faa5139030c60de6dd3bc69877d4f
Olga Adams <helga7777@gmail.com>
2023-06-12 19:03:42 -0600

    first lesson

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: "Shopping",
+  },
+  {
+    id: 2,
+    title: "Cleaning",
+  },
+  {
+    id: 3,
+    title: "Classes",
+  }
+  
+];
+
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
